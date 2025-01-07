3830c7e2cf664279817cceceebd6252c57e11f85
Radosław Pałka <radekpalka@gmail.com>
2025-01-07 17:50:14 +0100

    refactor: move update array logic from component to redux

--- src/state/trainingArrSlice.ts
+++ src/state/trainingArrSlice.ts
@@ -1,3 +1,4 @@
+import { TrainingObjectType } from '../types/TrainingObjectType';
 import { StageObj } from './../types/StageObj';
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
@@ -13,12 +14,59 @@ const trainingArrSlice = createSlice({
 	name: 'training-arr',
 	initialState,
 	reducers: {
-		setTrainingArr: (state, action: PayloadAction<StageObj[]>) => {
-			state.trainingArr = action.payload;
+		updateTrainingArr: (state, action: PayloadAction<TrainingObjectType>) => {
+			const {
+				exerciseLength,
+				restLength,
+				numberOfCycles,
+				numberOfSets,
+				restBetweenSets,
+			} = action.payload;
+			const arr: StageObj[] = [];
+			let cycleIndex = 0;
+
+			for (let j = 0; j < numberOfSets; j++) {
+				for (let i = 0; i < numberOfCycles; i++) {
+					cycleIndex = i + 1;
+
+					arr.push({
+						type: 'exercise',
+						length: exerciseLength,
+						setIndex: j + 1,
+						cycleIndex,
+					});
+
+					if (!restLength || i === numberOfCycles - 1) continue;
+
+					arr.push({
+						type: 'rest',
+						length: restLength,
+						setIndex: j + 1,
+						cycleIndex,
+					});
+				}
+
+				if (numberOfSets > 1 && j < numberOfSets - 1) {
+					arr.push({
+						type: 'rest-between-sets',
+						length: restBetweenSets || 0,
+						setIndex: j + 1,
+						cycleIndex,
+					});
+				} else if (j === numberOfSets - 1) {
+					arr.push({
+						type: 'done',
+						length: 0,
+						setIndex: j + 1,
+						cycleIndex,
+					});
+				}
+			}
+			state.trainingArr = arr;
 		},
 	},
 });
 
-export const { setTrainingArr } = trainingArrSlice.actions;
+export const { updateTrainingArr } = trainingArrSlice.actions;
 
 export default trainingArrSlice.reducer;
